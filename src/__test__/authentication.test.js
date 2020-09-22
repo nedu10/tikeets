1559b50bd099803495dee6d5c559fdecbc2ca978
Chinedu Ifediorah <cifediorah3@gmail.com>
2020-09-22 15:26:51 +0100

    feat, admin create and event and get all upcoming event test

--- src/__test__/authentication.test.js
+++ src/__test__/authentication.test.js
@@ -11,8 +11,9 @@ const jsonwebtoken = require("jsonwebtoken");
 const User = require("../App/Models/user.models");
 const RoleType = require("../App/Models/role.models");
 
+const Event = require("../App/Models/event.models");
+
 // our global object for storing auth information
-let data = {};
 let password = "joe_secret";
 const seed = [
   {
@@ -33,9 +34,9 @@ describe("POST /signup", () => {
     seed.forEach((each) => {
       var role_type = new RoleType(each);
       if (each.label == "ADMIN") {
-        data.admin_role_id = role_type._id;
+        process.env.admin_role_id = role_type._id;
       } else {
-        data.user_role_id = role_type._id;
+        process.env.user_role_id = role_type._id;
       }
       role_type
         .save()
@@ -48,7 +49,7 @@ describe("POST /signup", () => {
       first_name: "John",
       last_name: "Doe",
       email: "john5.doe@gmail.com",
-      role_id: data.user_role_id,
+      role_id: process.env.user_role_id,
       password: password,
     });
     expect(register.body.status_code).toBe(201);
@@ -62,10 +63,9 @@ describe("POST Admin /signup", () => {
       first_name: "Tikeet",
       last_name: "Admin",
       email: "admin@tikeets.com",
-      role_id: data.admin_role_id,
+      role_id: process.env.admin_role_id,
       password: password,
     });
-    // console.log(register);
     expect(register.body.status_code).toBe(201);
   });
 });
@@ -77,7 +77,7 @@ describe("POST /login", () => {
       email: "john5.doe@gmail.com",
       password,
     });
-    data.user_token = login.body.token;
+    process.env.user_token = login.body.token;
     expect(login.body.status_code).toBe(201);
     expect(login.body.token).toBeTruthy();
   });
@@ -89,18 +89,52 @@ describe("POST Admin /login", () => {
       email: "admin@tikeets.com",
       password,
     });
-    data.admin_token = login.body.token;
+    process.env.admin_token = login.body.token;
     expect(login.body.status_code).toBe(201);
     expect(login.body.token).toBeTruthy();
   });
 });
 
-afterAll(async () => {
-  //disconnect db
-  await User.remove({});
-  await RoleType.remove({});
-  //   console.log("data >> ", data);
-  db.disconnect();
+// Admin create event Test
+describe("POST /events", () => {
+  test("Admin creates an event", async () => {
+    const create_event = await request
+      .post("/events")
+      .set("authorization", process.env.admin_token)
+      .send({
+        name: "Google Conference",
+        location: "12, Saka Tinibu Street, Victoria Island, Lagos, Nigeria",
+        details: "2020 Google conference limited to 500 guest",
+        event_date: "2011-10-05T14:48:00.000Z",
+        reservation_limit: 500,
+      });
+    expect(create_event.body.status_code).toBe(201);
+  });
 });
 
-process.env.SETUP_DATA = data;
+// get all upcoming event Test
+describe("GET /events", () => {
+  test("get all upcoming event", async () => {
+    const create_event = await request
+      .get("/events")
+      .set("authorization", process.env.user_token);
+    expect(create_event.body.status_code).toBe(200);
+  });
+});
+
+// update event Test
+describe("PATCH /events/:event_id", () => {
+  test("get all upcoming event", async () => {
+    const create_event = await request
+      .get("/events")
+      .set("authorization", process.env.user_token);
+    expect(create_event.body.status_code).toBe(200);
+  });
+});
+
+afterAll(async () => {
+  await User.deleteMany({});
+  await Event.deleteMany({});
+  await RoleType.deleteMany({});
+  db.disconnect();
+});
