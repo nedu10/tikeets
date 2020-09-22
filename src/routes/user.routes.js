81e179d783bc903dc28be84eccebd96c27ecd84a
Chinedu Ifediorah <cifediorah3@gmail.com>
2020-09-22 02:45:45 +0100

    feat, implemented get all user reservations

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -3,6 +3,6 @@ const router = require("express").Router();
 const { user_reserve_ticket } = require("../App/Controllers/user.controllers");
 const { auth } = require("../App/Middleware/auth.middleware");
 
-router.post("/tickets", auth, user_reserve_ticket);
+router.get("/tickets", auth, user_reserve_ticket);
 
 module.exports = router;
