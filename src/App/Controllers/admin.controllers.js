43a7df1e3dff6e5c36081c98c59f7540373ced62
Chinedu Ifediorah <cifediorah3@gmail.com>
2020-09-22 02:59:23 +0100

    feat: Added get all roles endpoints

--- src/App/Controllers/admin.controllers.js
+++ src/App/Controllers/admin.controllers.js
@@ -1,4 +1,5 @@
 const Reservation = require("../Models/reservation.models");
+const Role = require("../Models/role.models");
 
 module.exports = {
   async user_reserve_ticket(req, res) {
@@ -12,6 +13,25 @@ module.exports = {
         status: "Successful",
         message: "Successfully fetched user reservations",
         results: get_my_reservations,
+      });
+    } catch (error) {
+      console.log("error >> ", error);
+      return res.status(500).json({
+        status_code: 500,
+        status: "Error",
+        message: "Opps! Something went wrong.",
+        error: error.message,
+      });
+    }
+  },
+  async view_all_roles(req, res) {
+    try {
+      const get_all_roles = await Role.find();
+      return res.status(200).json({
+        status_code: 200,
+        status: "Successful",
+        message: "Successfully fetched all user roles",
+        results: get_all_roles,
       });
     } catch (error) {
       console.log("error >> ", error);
--- src/routes/admin.routes.js
+++ src/routes/admin.routes.js
@@ -1,8 +1,12 @@
 const router = require("express").Router();
 
-const { user_reserve_ticket } = require("../App/Controllers/admin.controllers");
+const {
+  user_reserve_ticket,
+  view_all_roles,
+} = require("../App/Controllers/admin.controllers");
 const { admin_auth } = require("../App/Middleware/auth.middleware");
 
 router.get("/:user_id/tickets", admin_auth, user_reserve_ticket);
+router.get("/admin/roles", admin_auth, view_all_roles);
 
 module.exports = router;
